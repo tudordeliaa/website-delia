fd45497048d9926c24307c9e212636f59eecf1c4
tudor delia <tudordeliaa@gmail.com>
2021-03-31 11:44:41 +0300

    Initial commit. Setup main react structure

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Delia and Team
+          Learn React with Delia
         </a>
       </header>
     </div>
